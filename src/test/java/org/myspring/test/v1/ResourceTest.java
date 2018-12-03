8554a3b23b097215c7bf1c271cdd421cc3712522
yujt-moon <1291932524@qq.com>
2018-12-03 15:44:44 +0800

    添加注释并优化代码

--- src/test/java/org/myspring/test/v1/ResourceTest.java
+++ src/test/java/org/myspring/test/v1/ResourceTest.java
@@ -9,6 +9,7 @@ import org.myspring.core.io.Resource;
 import java.io.InputStream;
 
 /**
+ * 资源读取配置
  * @author yujiangtao
  * @date 2018/6/16 18:24
  */
@@ -18,24 +19,23 @@ public class ResourceTest {
     public void testClassPathResource() throws Exception {
         Resource r = new ClassPathResource("petstore-v1.xml");
 
-        InputStream is = null;
-        try {
-            is = r.getInputStream();
-            // 注意：这个测试其实并不充分！！
-            Assert.assertNotNull(is);
-            String description = r.getDescription();
-            Assert.assertNotNull(description);
-        } finally {
-            if(is != null) {
-                is.close();
-            }
-        }
+        testResourceExist(r);
     }
 
     @Test
     public void testFileSystemResource() throws Exception {
         Resource r = new FileSystemResource("src\\test\\resources\\petstore-v1.xml");
 
+        testResourceExist(r);
+
+    }
+
+    /**
+     * 测试改文件资源是否存在
+     * @param r
+     * @throws Exception
+     */
+    private void testResourceExist(Resource r) throws Exception {
         InputStream is = null;
 
         try {
@@ -49,6 +49,5 @@ public class ResourceTest {
                 is.close();
             }
         }
-
     }
 }

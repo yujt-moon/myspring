9be5f54997600dca5b5ae9538325db36173535e0
yujt@asiainfo.com <yujt@asiainfo.com>
2020-09-01 14:28:36 +0800

    注解注入

--- src/test/java/org/myspring/test/AllTests.java
+++ src/test/java/org/myspring/test/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.myspring.test.v1.V1AllTests;
 import org.myspring.test.v2.V2AllTests;
 import org.myspring.test.v3.V3AllTests;
+import org.myspring.test.v4.V4AllTests;
 
 /**
  * 所有测试用例的总测试
@@ -15,7 +16,8 @@ import org.myspring.test.v3.V3AllTests;
 @Suite.SuiteClasses({
         V1AllTests.class,
         V2AllTests.class,
-        V3AllTests.class
+        V3AllTests.class,
+        V4AllTests.class
 })
 public class AllTests {
 }
